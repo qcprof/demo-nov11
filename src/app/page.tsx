36d3ea5451c11e7117c87bb31d05505a340b8a1c
prof <152490665+isramasjid@users.noreply.github.com>
2024-11-11 19:07:48 -0500

    added new paragraph

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-4xl m-10 text-green-700">Acc 343 Homepage</h1>
+      <p className="m-20">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum totam, est minus dolor cumque enim magni? Iste ipsum explicabo quam in corporis vero id at cumque ducimus error maxime quo eum quis excepturi, quasi quidem ea officiis minus commodi saepe. Dolores consequatur ad magni aspernatur sapiente illum laboriosam nesciunt sequi!</p>
     </>
   );
 }
